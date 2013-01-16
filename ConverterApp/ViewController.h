84a18e9a0e75d8b3a74cd66b55cacb0e605f8745
nguillot <nguillot@ippon.fr>
2013-01-16 12:03:03 +0100

    Initial Commit

---
+++ ConverterApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ConverterApp
+//
+//  Created by nguillot on 16/01/13.
+//  Copyright (c) 2013 Ippon Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ConverterAppTests/ConverterAppTests.h
@@ -0,0 +1,13 @@
+//
+//  ConverterAppTests.h
+//  ConverterAppTests
+//
+//  Created by nguillot on 16/01/13.
+//  Copyright (c) 2013 Ippon Technologies. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ConverterAppTests : SenTestCase
+
+@end
